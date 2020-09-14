31d8d2f9bc5043473467618e0cdb5dff7650dd15
afsaneh afba. bajoori <afba@pc-tec.local>
2020-09-14 09:20:08 +0200

    Add project files.

---
+++ tryCatchFinally/ExceptionHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tryCatchFinally
+{
+    class ExceptionHandler
+    {
+        static int[] numbers = { 1, 5, 8, 4, 15 };
+        public static bool WrongAnswer ; // if user inter an index out of the range of index sende an error massege
+
+        public static int GetNumber(int index)
+        {
+            int output = -1; //we know that all of the numbers is positive
+ 
+            try
+            {
+                output= numbers[index];
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally //it is run after try or catch works
+            {
+                if (output == -1)
+                {
+                    
+                    WrongAnswer = true;
+                }
+                else
+                {
+                  
+                    WrongAnswer = false;
+                }
+                
+            }
+            return output;
+
+
+        }
+    }
+}
---
+++ tryCatchFinally/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace tryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //program ask a user to inter an index and program return a number from index
+
+            Console.WriteLine("Please inter an index!");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+
+            int result= ExceptionHandler.GetNumber(index);
+
+            if (ExceptionHandler.WrongAnswer ==true)
+            {
+                Console.WriteLine("The index was wrong!");
+            }
+            else
+            {
+
+                Console.WriteLine($"The number is {result}"); 
+            }
+            
+           
+
+            Console.WriteLine("Please a press a key to exit!");
+            Console.ReadKey(true);
+        }
+    }
+}
